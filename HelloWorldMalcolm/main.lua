463ee9fd2496d5b7c9b3bcd094d9d0b479e07991
s299108 <malcolm.cantin@ocsbstudent.ca>
2020-02-27 13:02:44 -0500

    Update main.lua

--- HelloWorldMalcolm/main.lua
+++ HelloWorldMalcolm/main.lua
@@ -13,13 +13,22 @@ print ("**** Hello Malcolm!")
 display.setStatusBar(display.HiddenStatusBar)
 
 -- sets the background colour
-display.setDefault("background", 124/255, 249/255, 199/255)
+display.setDefault("background", 175/255, 2/255, 120/255)
 
 -- create a local variable
 local textObject
 
--- displays text on the screen at position x = 500 and y = 500 with a default font style and font size of 50
-textObject = display.newText( "Hello, Malcolm!", 500, 500, nil, 50 )
+-- displays text on the screen at position x = 500 and y = 250 with a default font style and font size of 100
+textObject = display.newText( "Hello, Malcolm!", 500, 250, nil, 100)
 
 -- sets the color of the text
-textObject:setTextColor(155/255, 42/255, 198/255)
+textObject:setTextColor(189/255, 254/255, 254/255)
+
+-- create another local variable
+local textObjectSignature
+
+-- displays text on the screen at position x = 500 and y = 500 with a default font style and font size of 45 
+textObjectSignature = display.newText( "Created by: Malcolm Cantin", 500, 500, nil, 45)
+
+-- sets the color of the text signature
+textObjectSignature:setTextColor(66/255, 200/255, 245/255)
