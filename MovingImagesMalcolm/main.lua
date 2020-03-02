ce66191d20d20b3193f48322f5b4247a7a7781a9
s299108 <malcolm.cantin@ocsbstudent.ca>
2020-03-02 14:30:56 -0500

    MoveImages

---
+++ MovingImagesMalcolm/main.lua
@@ -0,0 +1,72 @@
+-----------------------------------------------------------------------------------------
+-- Title: MovingImages
+-- Name: Malcolm Cantin
+-- Course: ICS2O
+-- This program moves a beetleship across the screen and then makes it fade in. I added
+-- a second object that moves in a different direction, fades out and shirnks in size.
+-----------------------------------------------------------------------------------------
+-- Your code here
+
+-- CODE FOR THE BEETLESHIP
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+
+-- character image with width and height
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- set the image to be transparent
+beetleship.alpha = 0
+
+-- set the initial x and y position of beetleship
+beetleship.x = 0
+beetleship.y = display.contentHeight/3
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to the x-value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+	-- change the transparency of the ship every time it moves so that it fades out
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveShip)
+
+-- CODE FOR THE ROCKETSHIP
+
+-- character image with width and height
+local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
+
+-- set the image to be transparent
+rocketship.alpha = 1
+
+-- set the initial x and y position of rocketship
+rocketship.x = 1024
+rocketship.y = display.contentHeight*(2/3)
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the ship
+local function MoveRocket(event)
+	-- add the scroll speed to the x-value of the ship
+	rocketship.x = rocketship.x - scrollSpeed
+	-- change the transparency of the ship every time it moves so that it fades out
+	rocketship.alpha = rocketship.alpha - 0.000000001
+end
+
+-- MoveRocket will be called over and over again
+Runtime:addEventListener("enterFrame", MoveRocket)
+
+-- flip the rocket
+rocketship:scale(-1, 1)
+
